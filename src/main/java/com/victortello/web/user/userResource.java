b7f269464400dd5c0eb00eaa10dc283a6ded9cbf
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-21 12:34:55 -0600

    user not found exception

--- src/main/java/com/victortello/web/user/userResource.java
+++ src/main/java/com/victortello/web/user/userResource.java
@@ -26,7 +26,13 @@ public class userResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if (user == null){
+            throw new UserNotFoundException("id=" + id);
+        }
+
+        return user;
     }
 
     @PostMapping("/users")
