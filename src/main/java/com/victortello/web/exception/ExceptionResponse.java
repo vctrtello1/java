01ce15a2fe151926f8c89f480976d8a85ae11dfa
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-21 13:39:33 -0600

    exception handler

---
+++ src/main/java/com/victortello/web/exception/ExceptionResponse.java
@@ -0,0 +1,49 @@
+package com.victortello.web.exception;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+    
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    
+    public ExceptionResponse(Date timestamp, String message, String details) {
+        this.setTimestamp(timestamp);
+        this.setMessage(message);
+        this.setDetails(details);
+    }
+
+
+    public String getDetails() {
+        return details;
+    }
+
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    
+}
