aaa42bb8259029197c6752a519c74642db72aa22
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-21 12:06:41 -0600

    ResponseEntity for user request

--- src/main/java/com/victortello/web/user/userResource.java
+++ src/main/java/com/victortello/web/user/userResource.java
@@ -1,11 +1,16 @@
 package com.victortello.web.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class userResource {
@@ -23,5 +28,14 @@ public class userResource {
     public User retrieveUser(@PathVariable int id){
         return service.findOne(id);
     }
-    
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location =  ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
 }
