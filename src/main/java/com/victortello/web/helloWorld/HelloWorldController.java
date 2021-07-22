c4f6652c88e335549d231720849fd41b9ec3d73d
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-22 11:32:21 -0600

    LocaleHolderContex instead of RequestHeader

--- src/main/java/com/victortello/web/helloWorld/HelloWorldController.java
+++ src/main/java/com/victortello/web/helloWorld/HelloWorldController.java
@@ -3,9 +3,8 @@ package com.victortello.web.helloWorld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Locale;
 import org.springframework.context.MessageSource;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
@@ -30,9 +29,8 @@ public class HelloWorldController {
     }
 
     @GetMapping(path = "/hello-world-international")
-    public String helloWorldInternationalized(
-            @RequestHeader(name = "Accept-Lenguage", required = false) Locale locale) {
-        return messageSource.getMessage("good.morning.message", null,
-        "Default Message", locale);
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null, "Default Message",
+                LocaleContextHolder.getLocale());
     }
 }
