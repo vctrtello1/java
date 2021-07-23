23b3858e895c56339e09270f6d95129eb402d86d
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-22 21:16:49 -0600

    use of JPA with H2 database

--- src/main/java/com/victortello/web/user/User.java
+++ src/main/java/com/victortello/web/user/User.java
@@ -2,12 +2,14 @@ package com.victortello.web.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+@Entity
 public class User {
-
-    
 
     public User(Integer id, String name, Date birthDate) {
         this.id = id;
@@ -15,6 +17,8 @@ public class User {
         this.birthDate = birthDate;
     }
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characteres")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(10001, sysdate(), 'Hugo');
+insert into user values(10002, sysdate(), 'Victor');
+insert into user values(10003, sysdate(), 'TellO');
